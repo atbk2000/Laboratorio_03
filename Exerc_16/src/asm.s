3824dfe942448b5cf87d37f70c7407697504eb3a
atbk2000 <73497758+atbk2000@users.noreply.github.com>
2021-08-16 13:36:36 -0300

    Update asm.s

--- Exerc_16/src/asm.s
+++ Exerc_16/src/asm.s
@@ -1,16 +1,60 @@
-        PUBLIC  __iar_program_start
-        PUBLIC  __vector_table
+       PUBLIC  __iar_program_start
+       PUBLIC  __vector_table
 
-        SECTION .text:CODE:REORDER(1)
+       SECTION .text:CODE:REORDER(2)
         
-        ;; Keep vector table even if it's not referenced
-        REQUIRE __vector_table
+       ;; Keep vector table even if it's not referenced
+       REQUIRE __vector_table
         
-        THUMB
-        
+       THUMB
+
+SYSCTL_RCGCGPIO_R       EQU     0x400FE608
+SYSCTL_PRGPIO_R		EQU     0x400FEA08
+PORTN_BIT               EQU     1000000000000b ; bit 12 = Port N
+
+GPIO_PORTN_DATA_R    	EQU     0x40064000
+GPIO_PORTN_DIR_R     	EQU     0x40064400
+GPIO_PORTN_DEN_R     	EQU     0x4006451C
+
 __iar_program_start
         
-main    B       main
+main    MOV R2, #PORTN_BIT
+	LDR R0, =SYSCTL_RCGCGPIO_R
+	LDR R1, [R0] ; leitura do estado anterior
+	ORR R1, R2 ; habilita port N
+	STR R1, [R0] ; escrita do novo estado
+
+        LDR R0, =SYSCTL_PRGPIO_R
+wait	LDR R2, [R0] ; leitura do estado atual
+	TEQ R1, R2 ; clock do port N habilitado?
+	BNE wait ; caso negativo, aguarda
+
+        MOV R2, #00000001b ; bit 0
+        
+	LDR R0, =GPIO_PORTN_DIR_R
+	LDR R1, [R0] ; leitura do estado anterior
+	ORR R1, R2 ; bit de saída
+	STR R1, [R0] ; escrita do novo estado
+
+	LDR R0, =GPIO_PORTN_DEN_R
+	LDR R1, [R0] ; leitura do estado anterior
+	ORR R1, R2 ; habilita função digital
+	STR R1, [R0] ; escrita do novo estado
+
+        MOV R1, #000000001b ; estado inicial
+ 	LDR R0, = GPIO_PORTN_DATA_R
+        MOV R2, #0x3FC
+        
+        
+loop    LDR R4, [R0, R2]
+        EOR R4, R1
+        STR R4, [R0, R2]
+        MOVT R3, #0x000F ; constante de atraso 
+delay   CBZ R3, theend ; 1 clock
+        SUB R3, R3, #1 ; 1 clock
+        B delay ; 3 clocks
+theend  EOR R1, R1, R2 ; troca o estado
+        B loop
 
         ;; Forward declaration of sections.
         SECTION CSTACK:DATA:NOROOT(3)
@@ -71,3 +115,4 @@ __default_handler
         B __default_handler
 
         END
+
